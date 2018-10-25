7578a261f40c1724eec946a6513fcc342c4c2d1a
SigmaHyperon <nano_tech@web.de>
2018-10-25 16:47:14 +0200

    added error logging

--- modules/log.js
+++ modules/log.js
@@ -5,5 +5,13 @@ function debug(debugItem, sources){
     log("Debug:", sources);
     console.log(debugItem);
 }
+function error(message, sources){
+    sources.unshift("ERROR");
+    log(message, sources);
+}
+function debugError(debugItem, sources){
+    sources.unshift("ERROR");
+    debug(debugItem, sources);
+}
 
-module.exports = {log, debug};
+module.exports = {log, debug, error, debugError};
