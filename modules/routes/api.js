09e5ebf7c7bea1a2e3b3658b80a1bbd5f46cbde9
SigmaHyperon <nano_tech@web.de>
2018-10-25 15:26:34 +0200

    added dummy data to rest

--- modules/routes/api.js
+++ modules/routes/api.js
@@ -10,13 +10,26 @@ apiRouterV1.get(`/inv`, (req, res) => {
     res.send(`list of inventories`);
 });
 apiRouterV1.get(`/inv/:invId`, (req, res) => {
-    res.send(`Inventory #${req.params.invId} Overview`);
+    res.json({
+        id: "c6754d2d-ee14-4e85-86ca-35dbfbbb9024",
+        name: "backpack",
+        user: "42"
+    });
+    //res.send(`Inventory #${req.params.invId} Overview`);
 });
 apiRouterV1.get(`/inv/:invId/item`, (req, res) => {
     res.send(`Inventory #${req.params.invId} Item Overview`);
 });
 apiRouterV1.get(`/inv/:invId/item/:itemId`, (req, res) => {
-    res.send(`Inventory #${req.params.invId} Item #${req.params.itemId}`);
+    res.json({
+        id: "9792c4f0-3038-466c-b657-d0b8e58ca041",
+        name: "Pretty cool Sword",
+        amount: 1,
+        description: "a pretty cool sword",
+        icon: "42",
+        Inventory: "42"
+    });
+    //res.send(`Inventory #${req.params.invId} Item #${req.params.itemId}`);
 });
 
 var apiRouter = express.Router();
