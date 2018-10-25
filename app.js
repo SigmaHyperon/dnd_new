5c0f3e4cd1a5afc97f15c67178102ce4b90e4781
SigmaHyperon <nano_tech@web.de>
2018-10-25 20:10:09 +0200

    added separate config options

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const log = require('./modules/log.js');
 const apiRouter = require('./modules/routes/api.js');
+const config = require('config');
 var app = express();
-const port = 80;
+const port = config.has('port') ? config.get('port') : 80;
 
 app.use(`/inv`, express.static("www"));
 app.use(`/api`, apiRouter);
