989cd84796742eea58805483c28c9967bec8748d
SigmaHyperon <nano_tech@web.de>
2018-11-07 16:55:06 +0100

    added object create and delete

--- www/js/conn.js
+++ www/js/conn.js
@@ -5,6 +5,18 @@ class GameObject {
         this.internals.connection = connection;
         this.internals.isLoaded = false;
     }
+    clean(){
+        let cleanAttributes = Object.keys(this).filter(v => {
+            return (v !== 'internals' && typeof this[v] !== 'function');
+        });
+        let ret = {};
+        for (var el in cleanAttributes) {
+            if (cleanAttributes.hasOwnProperty(el)) {
+                ret[cleanAttributes[el]] = this[cleanAttributes[el]];
+            }
+        }
+        return ret;
+    }
     async load(id){
         let result = await this.internals.connection.request('GET', `${this.internals.path}/${id}`);
         let data;
@@ -22,17 +34,7 @@ class GameObject {
     async save(){
         if(this.internals.isLoaded !== true)
             throw "object not loaded yet";
-
-        let cleanAttributes = Object.keys(this).filter(v => {
-            return (v !== 'internals' && typeof this[v] !== 'function');
-        });
-        let toTransfer = {};
-        for (var el in cleanAttributes) {
-            if (cleanAttributes.hasOwnProperty(el)) {
-                toTransfer[cleanAttributes[el]] = this[cleanAttributes[el]];
-            }
-        }
-        let result = await this.internals.connection.request('PATCH', `${this.internals.path}/${this.id}`, toTransfer);
+        let result = await this.internals.connection.request('PATCH', `${this.internals.path}/${this.id}`, this.clean());
     }
     async list(){
         let result = await this.internals.connection.request('GET', `${this.internals.path}`);
@@ -51,6 +53,17 @@ class GameObject {
             }
         }
         return list;
+    }
+    async create(){
+        if(this.internals.isLoaded !== false)
+            throw 'object already exists';
+        let result = await this.internals.connection.request('PUT', this.internals.path, this.clean());
+        this.loadData(result);
+    }
+    async delete(){
+        if(this.internals.isLoaded !== true)
+            throw "object not loaded yet";
+        let result = await this.internals.connection.request('DELETE', `${this.internals.path}/${this.id}`);
     }
 }
 class User extends GameObject {
@@ -105,11 +118,11 @@ function init_conn(){
             throw 'not connected';
             return;
         }
-        let settings = {
-            method: method,
-            data: JSON.stringify(data),
-            contentType: 'application/json'
-        };
+        let settings = {method};
+        if(typeof data !== 'undefined'){
+            settings.data = JSON.stringify(data);
+            settings.contentType = 'application/json';
+        }
         if(location.protocol == 'https')
             settings.headers = {"x-auth": JSON.stringify({user: this.credentials.userId, token: this.credentials.token.secret})};
         return await $.ajax(`/api/v1/${path}`, settings);
